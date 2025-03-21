d6d98c6585ce831cb774d6a818b34b102326cf12
Abhay Jagadish Agasibagalavar <91181562+Abhayja@users.noreply.github.com>
2025-03-21 22:37:43 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// Replace 'job-application-form' with your repository name
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/job-application-form/',
+});
